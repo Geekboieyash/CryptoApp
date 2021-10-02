8e29c8b158f92d968925f2f01157a0cfd46f7dd8
Yash Mishra <89203577+Geekboieyash@users.noreply.github.com>
2021-10-02 12:24:23 +0530

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png' ;
 
 const Navbar = () => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(null);
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+
+    useEffect(() => {
+       if(screenSize < 768){
+           setActiveMenu(false);
+       } else {
+           setActiveMenu(true);
+       }
+    }, [screenSize])
+
+
+
     return (
         <div className='nav-container'>
             <div className="logo-container">
@@ -12,8 +31,12 @@ const Navbar = () => {
                 <Typography.Title leve={2} className="logo">
                     <Link to="/">CryptoTalk</Link>
                 </Typography.Title>
+                <Button className="menu-control-container" onClick={() =>setActiveMenu(!activeMenu)}>
+                    <MenuOutlined />
+                </Button>
             </div>
-            <Menu theme="dark">
+            {activeMenu && (
+                <Menu theme="dark">
                 <Menu.Item icon={<HomeOutlined/>}>
                     <Link to="/">Home</Link>
                 </Menu.Item>
@@ -28,6 +51,9 @@ const Navbar = () => {
                 </Menu.Item>
 
             </Menu>
+
+            )}
+            
         </div>
     )
 }
